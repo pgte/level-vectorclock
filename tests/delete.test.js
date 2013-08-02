55d8ec6f561b1c51e1a9f3668bacbbb446d1b185
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-08-02 18:36:50 +0100

    missing delete test file

---
+++ tests/delete.test.js
@@ -0,0 +1,48 @@
+var assert = require('assert');
+var test   = require('tap').test;
+var utils  = require('./utils');
+var LVC    = require('../');
+
+var path = __dirname + '/.testdbs/putandget'
+var db = utils.setup(path, 'node1');
+
+test('puts one', function(t) {
+  db.put('key1', 'value1', function(err) {
+    if (err) throw err;
+    t.end();
+  });
+});
+
+test('gets one', function(t) {
+  db.get('key1', function(err, recs) {
+    if (err) throw err;
+    var expected = [{key: 'key1', value: 'value1', meta: { clock: { node1: 1}}}];
+    t.deepEqual(recs, expected);
+    t.end();
+  });
+});
+
+test('deletes', function(t) {
+  db.del('key1', function(err) {
+    if (err) throw err;
+    t.end();
+  });
+});
+
+test('value is no longer present', function(t) {
+  db.get('key1', function(err, values) {
+    if (err) throw err;
+    t.equal(values.length, 0);
+    t.end();
+  });
+});
+
+test('closes', function(t) {
+  db.close(t.end.bind(t));
+});
+
+function sort(a, b) {
+  if (a.key < b.key) return -1;
+  if (a.value < b.value) return -1;
+  return 1;
+}
